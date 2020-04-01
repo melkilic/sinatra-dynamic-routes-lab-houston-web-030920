72bcec1c3d951cff84239e1a277aa977e1988f99
melkilic <melike180400@gmail.com>
2020-03-31 20:07:20 -0500

    Done.

--- app.rb
+++ app.rb
@@ -27,20 +27,6 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    # @number1= params[:number1].to_i
-    # @number2=params[:number2].to_i
-    # # @operation= params[:operation]
-    # case params[:operation]
-    # when 'add'
-    #   result= (@number1 + @number2).to_s
-    # when 'substract'
-    #   result= (@number1 - @number2).to_s
-    # when 'multiply'
-    #   result= (@number1 * @number2).to_s
-    # when 'divide'
-    #   result= (@number1 / @number2).to_s
-
-
     n1 = params[:number1].to_i
 		n2 = params[:number2].to_i
 
